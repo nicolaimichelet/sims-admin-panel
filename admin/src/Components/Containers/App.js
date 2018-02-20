fff0202c4ae7759e7bfc3c39b06ab9cca38651ee
mariehessen <mariehessen@users.noreply.github.com>
2018-02-20 16:38:06 +0100

    added nav-bar
    Added navbar to the the website for more overview.

--- admin/src/Components/Containers/App.js
+++ admin/src/Components/Containers/App.js
@@ -4,10 +4,31 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <h1>Hei</h1>
-      </div>
-    );
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+          <a class="navbar-brand" href="#">SIMS</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Country <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Service</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">About</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+  );
   }
 }
 
