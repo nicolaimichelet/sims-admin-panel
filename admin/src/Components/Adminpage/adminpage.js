d795afbd1e977bfd97ca91958d7abb4623d0435a
eskilsen <eskil_hognestad@hotmail.com>
2018-02-21 16:09:31 +0100

    fetch start, can get things from a textfile.

--- admin/src/Components/Adminpage/adminpage.js
+++ admin/src/Components/Adminpage/adminpage.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import '../Containers/App.css';
+import {API} from '../../api.js';
+
+const DEFAULT_QUERY = 'redux';
 
 export default class adminpage extends Component {
     constructor(props){
         super(props);
+    
         this.state = {
-            text: "Initial text"
+            text: "Initial text",
+            hits: [],
         };
     }
-    componentWillMount(){
-
+    componentDidMount(){
+    this.setState({hits: API.hits})
     }
 
+
+
     render() {
+        const {hits} = this.state;
         return (
             <div>
                 {this.state.text}
+                {hits.map(hit =>
+                    <div key={hit.objectID}>
+                    <a href={hit.url}>{hit.title}</a>
+                </div>    
+            )
+                }
             </div>
+
+
         );
     }
 }
--- admin/src/Components/Containers/Header.js
+++ admin/src/Components/Containers/Header.js
@@ -4,25 +4,25 @@ export default class Header extends Component {
     render (){
         return(
             <div>
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-                    <a class="navbar-brand" href="/Adminpage">SIMS</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <a className="navbar-brand" href="/Adminpage">SIMS</a>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarColor01">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Country <span class="sr-only">(current)</span></a>
+                    <div className="collapse navbar-collapse" id="navbarColor01">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="#">Country <span className="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Service</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Service</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Pricing</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Pricing</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">About</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">About</a>
                             </li>
                         </ul>
                     </div>
