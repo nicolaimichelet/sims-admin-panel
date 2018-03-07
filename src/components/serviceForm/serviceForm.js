f91e2e8050a3e2212252c240b6020f27590d29a4
Nicolai <nicolai_m404@hotmail.com>
2018-03-07 14:23:00 +0100

    more form, started work on module

--- src/components/serviceForm/serviceForm.js
+++ src/components/serviceForm/serviceForm.js
@@ -1,49 +1,53 @@
 import React, { Component } from 'react';
 import '../containers/App.css';
 import '../css/serviceForm.css';
+import serviceModule from './serviceModule';
 
-
-const DEFAULT_QUERY = 'redux';
 
 export default class ServiceForm extends Component {
     constructor(props){
         super(props);
 
+        this.state = { isOpen: false };
     }
+
+    toggleModal = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
     componentDidMount(){
 
     }
-
-
 
     render() {
         return (
             <div>
                 <div>
                     <form>
-                        <fieldset>
-                            <legend>Add new service</legend>
-                            <div class="form-group">
+                        <fieldset className="form">
+                            <h2>Add new service</h2>
+                            <div class="field" class="form-group">
                             <label for="exampleInputName">Name</label>
                             <input type="name" class="form-control" id="exampleName" aria-describedby="emailHelp" placeholder="Enter name of service"/>
                             </div>
-                            <legend>Related Party</legend>
-                            <div class="form-group">
+                            <h5>Related party</h5>
+                            <div id="relatedParty" className="form-group">
                                 <label for="exampleInputRole">Role</label>
                                 <input type="role" class="form-control" id="exampleRole" aria-describedby="emailHelp" placeholder="Enter role"/>
                             </div>
-                            <div class="form-group">
+                            <div id="relatedParty" class="form-group">
                                 <label for="exampleInputID">ID</label>
                                 <input type="id" class="form-control" id="exampleID" aria-describedby="emailHelp" placeholder="Enter ID"/>
                             </div>
-                            <div class="form-group">
+                            <div id="relatedParty" class="form-group">
                                 <label for="exampleInputHref">HREF</label>
-                                <input type="name" class="form-control" id="exampleHref" aria-describedby="emailHelp" placeholder="href"/>
+                                <input type="name" class="form-control" id="exampleHref" aria-describedby="emailHelp" placeholder="Enter href"/>
                             </div>
-                            <div class="form-group">
-                            <label for="exampleTextarea">Description</label>
-                            <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
-                            </div>
+
+
+
                             {/*<fieldset class="form-group">
                             <legend>Radio buttons</legend>
                             <div class="form-check">
@@ -80,6 +84,8 @@ export default class ServiceForm extends Component {
                                 </label>
                             </div>
                             </fieldset>*/}
+
+                            <button onClick={this.toggleModal} id="advanced" type="module" class="btn btn-outline-secondary">Advanced</button>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </fieldset>
                         </form>
