a68eccd54fc8b61d4723e8cecf7ec795e8b75aad
mariehessen <mariehessen@users.noreply.github.com>
2018-03-09 23:09:44 +0100

    fixed navbar

--- src/components/containers/Header.js
+++ src/components/containers/Header.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
+import 'assets/css/header.css';
 import {Tab, Tabs} from 'material-ui/Tabs';
 import Slider from 'material-ui/Slider';
 
@@ -10,27 +11,19 @@ export default class Header extends Component {
     render (){
 
         const buttons = [
-            <FlatButton>
-                <a href="/AdminPage">Services</a>
-            </FlatButton>,
-            <FlatButton>
-                Places
-            </FlatButton>,
-            <FlatButton>
-                <a href="/ServiceForm">New Service</a>
-            </FlatButton>
+            <FlatButton  hoverColor='transparent' labelStyle={{color: 'white'}} href="/AdminPage" label="Service" />,
+            <FlatButton  hoverColor='transparent' labelStyle={{color: 'white'}} href="/" label="Place" />,
+            <FlatButton  hoverColor='transparent' labelStyle={{color: 'white'}} href="/ServiceForm" label="New Service" />
         ]
 
         return(
             <AppBar
                 title="SIMS"
+                titleStyle={{marginLeft: '30%', fontSize: '220%' }}
                 iconElementLeft={
-                    <FlatButton>{buttons}</FlatButton>
-                }
-            >
-        
+                    <FlatButton hoverColor='transparent'>{buttons}</FlatButton>
+                }/>
 
-            </AppBar>
             
 
         )
