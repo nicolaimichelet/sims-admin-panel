2e0eba8f7c2d3a8bfd17bb83dc58e3d6e924a06e
Jarle Trollebø <aborysa@gmail.com>
2018-03-11 04:46:18 +0100

    Added css module loading for production config aswell

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -183,6 +183,8 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
+                        localIdentName: '[name]__[local]___[hash:base64:5]',
+                        modules: true,
                       },
                     },
                     {
