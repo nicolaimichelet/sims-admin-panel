a88f8ae86144291e92d6a6e4631c6026dfbb242b
Jarle Trollebø <aborysa@gmail.com>
2018-03-11 08:10:54 +0100

    Removed devtools

--- public/main.js
+++ public/main.js
@@ -9,7 +9,6 @@ app.on('ready', () => {
   mainWindow = new BrowserWindow({ width: 1024, height: 728 });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-  mainWindow.webContents.openDevTools();
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
