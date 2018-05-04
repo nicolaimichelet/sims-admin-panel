579667475af17d56d0e9b0be6d166d4b38687063
Aborysa <aborysa@gmail.com>
2018-05-04 14:22:14 +0200

    Added check for controlled, should make component a bit more efficient when not controlled

--- src/components/containers/ServiceForm/ObjectInput.js
+++ src/components/containers/ServiceForm/ObjectInput.js
@@ -30,6 +30,10 @@ export class ObjectInput extends Component{
     this._updateFields(nextProps);
   }
 
+  isControlled(){
+    return this.props.value != null;
+  }
+
   _updateFields(props){
     let nmap = {};
     if(props.value != null){
@@ -73,7 +77,7 @@ export class ObjectInput extends Component{
 
         return React.cloneElement(field, {
           onChange: onChange,
-          value: get(this.state.value, field.props.name),
+          value: this.isControlled() ? get(this.state.value, field.props.name) : undefined,
           key: field.key || field.props.key || idx
         });
       }
