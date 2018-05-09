96561c1e67a4399c27d4700045ed34d5cb711343
eskilsen <eskil_hognestad@hotmail.com>
2018-05-09 12:37:49 +0200

    added more functionality for DetailView page. Missing supportingService and supportingResource

--- src/components/containers/DetailView.js
+++ src/components/containers/DetailView.js
@@ -7,7 +7,7 @@ import { mapAndConnect, IManagedService } from 'services';
 export class DetailView extends Component {
     constructor(props){
         super(props);
-        this.state = {service: {}}
+        this.state = {service: null}
     }
 
     componentDidMount(){
@@ -19,19 +19,34 @@ export class DetailView extends Component {
           })
         }, (err) => {
           this.setState({
-            errorText: err instanceof Response ? `HTTP ERROR: ${err.status} - ${err.statusText}` : "Could not load service" 
+            errorText: err instanceof Response ? `HTTP ERROR: ${err.status} - ${err.statusText}` : "Could not load service"
           })
         });
       }
 
     render(){
+        if (!this.state.service){
+            return <div/>;
+        }
         let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const partyList =[];
+        for (let party of this.state.service.getRelatedParty()){
+                partyList.push(<ul style = {{listStyleType: "none"}}>
+                <li>Name: {party.name}</li>
+                <li>Role: {party.role}</li>
+                <li>HREF: {party.href}</li>
+                </ul>
+            )
+        }
+
         return(
             <div>
+                <h2>{this.state.service.name}</h2>
                 <Paper>
                     <Paper>
-                        <h3>Basic:</h3>
-                        <ul>
+                        <h4>Basic:</h4>
+                        <ul style = {{listStyleType: "none"}}>
+                        <li>ID: {this.state.service.id}</li>
                         <li>Description: {this.state.service.description} </li>
                         <li>Order date: {this.state.service.orderDate ? this.state.service.orderDate.toLocaleDateString('en-US', options) : "None"}</li>
                         <li>Start date: {this.state.service.startDate ? this.state.service.startDate.toLocaleDateString('en-US', options) : "None"}</li>
@@ -43,14 +58,33 @@ export class DetailView extends Component {
                         <li>Status: {this.state.service.state}</li>
                         </ul>
                     </Paper>
-                    <Paper>Service spesification</Paper>
-                    <Paper>Related party:</Paper>
-                    <Paper>Supporting resource:</Paper>
-                    <Paper>Supporting service</Paper>
-                    ID:
-
-
-
+                    <Paper>
+                    <h4>Service specification</h4>
+                        <ul style = {{listStyleType: "none"}}>
+                        <li>{this.state.service.getServiceSpecification().name}</li>
+                        </ul>
+                    </Paper>
+                    <Paper>
+                    <h4>Related party:</h4>
+                    {partyList}
+                    </Paper>
+                    <Paper>
+                    <h4>Supporting service:</h4>
+                    <ul style = {{listStyleType: "none"}}>
+                    <li>ID: </li>
+                    <li>HREF: </li>
+                    <li>Name: </li>
+                    <li>Category: </li>
+                    </ul>
+                    </Paper>
+                    <Paper>
+                    <h4>Supporting resource:</h4>
+                    <ul style = {{listStyleType: "none"}}>
+                    <li>ID:</li>
+                    <li>HREF: </li>
+                    <li>Name: </li>
+                    </ul>
+                    </Paper>
                 </Paper>
             </div>
         );
